b4c78961997ec02712064c4f08174371ab1e1383
luiz.calcagnoto <luiz.calcagnoto@escolaconquer.com.br>
2023-01-29 09:37:23 -0300

    initial commit

---
+++ address-api/src/test/java/br/com/course/addressapi/AddressApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.addressapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ customer-api/src/test/java/br/com/course/customerapi/CustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.customerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
